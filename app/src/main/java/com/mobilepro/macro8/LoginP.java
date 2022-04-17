00723405323dff55ab1ee1b355846fbf7b0b05a7
DK-ops <321andika@gmail.com>
2022-04-18 00:02:58 +0700

    tambah splashscreen to login

---
+++ app/src/main/java/com/mobilepro/macro8/LoginP.java
@@ -0,0 +1,14 @@
+package com.mobilepro.macro8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_p);
+    }
+}
